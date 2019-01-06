19595c59a6081ce7cd8c6d11b64b803e3d132055
m3Ss1 <daniele.cavadini@gmail.com>
2019-01-06 15:44:31 +0100

    Added scripts in main folder

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-echo "Stopping running node instance"
 forever stop server.js
-
-echo "Fetching GIT repository"
-git fetch --all
-
-echo "Reset local to origin/master status"
+git fetch --all --quiet
 git reset --hard origin/master
-
-echo "Starting node..."
 forever start --minUptime 1000 --spinSleepTime 1000 server.js
